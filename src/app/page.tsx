71d747fa12c894d34544ea4681846d227064d8fa
ImVarity <JoshuaLazarte2004@gmail.com>
2024-10-13 04:14:38 -0700

    speech

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,20 @@ import { getLocation } from "@/tmobile/main";
 import Image from "next/image";
 import { getLLM } from "@/bedrock/main";
 import TextBox from "@/app/components/TextBox";
+import SpeechToText from "@/audio/speech-to-text";
+import TextToSpeech from "@/audio/text-to-speech";
 
 export default async function Home() {
   const location = await getLocation();
+
+  console.log(location)
 
 
 
   return (
     <div className="flex justify-center items-center flex-col">
+      <SpeechToText />
+      <TextToSpeech />
       <TextBox />
     </div>
 
